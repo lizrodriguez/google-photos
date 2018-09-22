520d8aafa98e8755d8fc3f2bd39860a2620fb6f6
Liz Rodriguez <liz@lizrodriguez.io>
2018-09-22 16:43:18 -0400

    added client config and dotenv

--- REST/PhotoFrame/config.js
+++ REST/PhotoFrame/config.js
@@ -13,14 +13,15 @@
 // limitations under the License.
 
 // This file contains the configuration options for this sample app.
+require('dotenv').config()
 
 const config = {};
 
 // The OAuth client ID from the Google Developers console.
-config.oAuthClientID = 'ADD YOUR CLIENT ID';
+config.oAuthClientID = process.env.CLIENTID;
 
 // The OAuth client secret from the Google Developers console.
-config.oAuthclientSecret = 'ADD YOUR CLIENT SECRET';
+config.oAuthclientSecret = process.env.CLIENTSECRET;
 
 // The callback to use for OAuth requests. This is the URL where the app is
 // running. For testing and running it locally, use 127.0.0.1.
